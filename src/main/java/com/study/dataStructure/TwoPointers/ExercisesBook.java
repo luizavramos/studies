69c1ed2f502654ba972ab3b013ec18b139604ea6
luizavramos <luizavramos30@gmail.com>
2025-04-24 12:20:09 -0300

    3 parallel pointers

--- src/main/java/com/study/dataStructure/TwoPointers/ExercisesBook.java
+++ src/main/java/com/study/dataStructure/TwoPointers/ExercisesBook.java
@@ -1,5 +1,6 @@
 package com.study.dataStructure.TwoPointers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExercisesBook {
@@ -19,6 +20,30 @@ public class ExercisesBook {
         }
 
         return false;
+    }
+    //27.8
+    public List<Integer> mergeWithOutDuplicates(List<Integer> arr1,List<Integer> arr2,List<Integer> arr3){
+        int p1 = 0, p2 = 0, p3 = 0;
+
+        List<Integer> response = new ArrayList<>();
+
+        while(p1 < arr1.size() || p2 < arr2.size() || p3 < arr3.size()){
+
+            int val1 = p1 < arr1.size() ? arr1.get(p1) : Integer.MAX_VALUE;
+            int val2 = p1 < arr1.size() ? arr1.get(p1) : Integer.MAX_VALUE;
+            int val3 = p1 < arr1.size() ? arr1.get(p1) : Integer.MAX_VALUE;
+
+            int minValue = Math.min(val1, Math.min(val2, val3));
+
+            if(response.isEmpty() || response.get(response.size() - 1) != minValue){
+                response.add(minValue);
+            }
+
+            if(val1 == minValue) p1++;
+            if(val2 == minValue) p2++;
+            if(val3 == minValue) p3++;
+        }
+        return response;
     }
 
 }
