0a8c193126fa784b47d8c3be090a157f577cab08
luizavramos <luizavramos30@gmail.com>
2025-04-24 12:25:43 -0300

    fix

--- src/main/java/com/study/dataStructure/TwoPointers/ExercisesBook.java
+++ src/main/java/com/study/dataStructure/TwoPointers/ExercisesBook.java
@@ -1,6 +1,7 @@
 package com.study.dataStructure.TwoPointers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ExercisesBook {
@@ -27,11 +28,11 @@ public class ExercisesBook {
 
         List<Integer> response = new ArrayList<>();
 
-        while(p1 < arr1.size() || p2 < arr2.size() || p3 < arr3.size()){
+        while(p1 < arr1.size() || p2 < arr2.size()|| p3 < arr3.size()){
 
             int val1 = p1 < arr1.size() ? arr1.get(p1) : Integer.MAX_VALUE;
-            int val2 = p1 < arr1.size() ? arr1.get(p1) : Integer.MAX_VALUE;
-            int val3 = p1 < arr1.size() ? arr1.get(p1) : Integer.MAX_VALUE;
+            int val2 = p2 < arr2.size() ? arr2.get(p2) : Integer.MAX_VALUE;
+            int val3 = p3 < arr3.size() ? arr3.get(p3) : Integer.MAX_VALUE;
 
             int minValue = Math.min(val1, Math.min(val2, val3));
 
@@ -44,6 +45,16 @@ public class ExercisesBook {
             if(val3 == minValue) p3++;
         }
         return response;
+    }
+    public static void main(String[] args) {
+        List<Integer> arr1 = Arrays.asList(1, 3, 5, 7, 9);
+        List<Integer> arr2 = Arrays.asList(2, 3, 6, 9, 10);
+        List<Integer> arr3 = Arrays.asList(1, 4, 5, 8, 10, 11,12);
+
+        ExercisesBook exercises = new ExercisesBook();
+        List<Integer> resultado = exercises.mergeWithOutDuplicates(arr1, arr2, arr3);
+
+        System.out.println("Merge sem duplicatas: " + resultado);
     }
 
 }
